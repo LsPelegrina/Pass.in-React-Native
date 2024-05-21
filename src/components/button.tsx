c7c8990cd530e789a208505f8eaa941a838975c0
LsPelegrina <lspelegrina@gmail.com>
2024-05-21 05:42:32 -0300

    fix--1

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, ActivityIndicator, TouchableOpacityProps} from "react-native"
+import { Text, TouchableOpacity, TouchableOpacityProps, ActivityIndicator} from "react-native"
 
 type Props = TouchableOpacityProps & {
   title: string
@@ -7,16 +7,19 @@ type Props = TouchableOpacityProps & {
 
 export function Button({ title, isLoading = false, ...rest }: Props) {
   return (
-    <TouchableOpacity 
+    <TouchableOpacity
       activeOpacity={0.7}
       disabled={isLoading}
       className="w-full h-14 bg-orange-500 items-center justify-center rounded-lg"
       {...rest}
     >
-        { isLoading ? ( <ActivityIndicator className="text-green-500"/>
-        ) : (
-          <Text className="">{title}</Text>
-        )}
+      {isLoading ? (
+        <ActivityIndicator className="text-green-500" />
+      ) : (
+        <Text className="text-green-500 text-base font-bold uppercase">
+          {title}
+        </Text>
+      )}
     </TouchableOpacity>
   )
 }
