79db265026597ede5bd9f7b0bd6134e5814ee626
LsPelegrina <lspelegrina@gmail.com>
2024-05-21 04:25:51 -0300

    Finalização da home page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,26 @@
-import "../styles/global.css"
+import "@/styles/global.css"
 
 import { Slot } from "expo-router"
 
-import { StatusBar } from "expo-status-bar"
+import { Loading } from "@/components/loading"
+
+import { useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto"
 
 export default function Layout(){
-  return (
-    <>
-      <StatusBar style="light"/>
-      <Slot/>
-    </>
-  )
+
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return <Slot/>
 }
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,5 +1,5 @@
 import { View, Image, StatusBar } from "react-native"
-import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
 import { Link } from "expo-router"
 
 import { colors } from "@/styles/colors"
@@ -7,27 +7,36 @@ import { colors } from "@/styles/colors"
 import { Input } from "@/components/input"
 import { Button } from "@/components/button"
 
-export default function Home(){
+export default function Register(){
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content"/>
+
       <Image source={require("@/assets/logo.png")} 
       className="h-16" 
       resizeMode="contain"
       />
+
       <View className="w-full mt-12 gap-3">
         <Input>
-          <MaterialCommunityIcons 
-            name="ticket-confirmation-outline"
+          <FontAwesome6 
+            name="user-circle"
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          <Input.Field placeholder="Nome completo" />
+        </Input>
+        <Input>
+          <MaterialIcons 
+            name="alternate-email"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field placeholder="E-mail" keyboardType="email-address" />
         </Input>
 
-        <Button 
-          title="Acessar credencial"
-        />
+        <Button title="Acessar credencial"/>
+
         <Link 
           href="/register" 
           className="text-gray-100 text-base font-bold 
